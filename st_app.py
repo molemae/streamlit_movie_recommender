5cd66a18e31ed7df111f8df7e15723270178952f
Moe <moritz.maehrlein@posteo.de>
2023-03-20 17:01:42 +0100

    fixing upstream mix up

--- st_app.py
+++ st_app.py
@@ -6,133 +6,78 @@ import seaborn as sns
 
 import streamlit as st
 
-import pickle
-
-df = pd.read_csv("./data/Ratings.csv")
-
-MODEL_FILE = "data/nmf_model2.pkl"
+from  recommender import recommend_random, recommend_with_NMF, recommend_neighborhood, ratings
 
 
-st.title("Welcome to Moe's Movie Recommender - Streamlit Edition")
+
+st.markdown(f"""
+    <style>
+    .stApp{{
+        background-image: url('https://smit.vub.ac.be/wp-content/uploads/2021/11/felix-mooneeram-evlkOfkQ5rE-unsplash-scaled.jpg');
+        background-size: cover;
+    }}
+    </style>
+    """,unsafe_allow_html=True)
+
 
 nav = st.sidebar.radio(
     " ",
-    ["Home", "Recommended"]
+    ["Home", "Wombats"]
     ) 
 
 if nav == "Home":
-    st.markdown(
-    """ ## Welcome to the Titanic Predictor page.
-    """
-    )
-    st.image("static/cat2.gif", width=400)
-    
-    st.markdown(
-    """ ### This model predicts if a person with specific traits has survived the ship's sink.
-    """
-    )
-
-if nav == "Recommended":
-    st.write("Here are your recommendations")
-#     st.write("Welcome to the section on Exploratory Data Analysis.")
-#     st.write("Data is taken from [titanic](https://www.kaggle.com/competitions/titanic/data?select=train.csv)")
-#     if st.checkbox("Click here to see the original dataframe"):
-#         st.title("Titanic training dataset")
-#         st.dataframe(df)
-#     if st.checkbox("Click here to see missing values"):
-#         fig,ax = plt.subplots()
-#         st.title("Heatmap features titanic dataset")
-#         sns.heatmap(df.corr(), ax = ax)
-#         st.write(fig)
-#     if st.checkbox("Click here to see distribution of variables"):
-#         st.title("Distribution features titanic dataset")
-#         fig = sns.pairplot(df, hue = "Survived")
-#         st.pyplot(fig)
-#     if st.checkbox("Click here to see the variables taken into account for model fit"):
-#         st.title("Features for model fit")
-#         st.write(df[['Pclass', 'Sex', 'Age', 'SibSp', 'Parch']].columns)
-
-# if nav == "Prediction":
-#     st.markdown(
-#     """ #### Welcome to the predictions page.
-#     """
-#     )
+    st.title(":red[Welcome to Simoritztinis Movie Recommender - Streamlit Edition]")
+    st.image("static/cat2.gif")
+    st.markdown(' ')
+    st.markdown(' ')
+    model_choice = st.selectbox('Select Recommedation Model', ['Random', 'NMF','Cosine Similarity'])
+    #pred = st.button('Get Recommendations', on_click(get_rec = True))
    
-#     st.markdown(
-#     """ ##### For predictions regarding if a passenger survived or not, please answer the following questions. 
-#     """
-#     )
+    if model_choice !='Random':
+        col1, col2, col3, col4 = st.columns(4)
+        movie0 = col2.selectbox(":red[First movie]", ratings.columns)
+        rating0 = col3.number_input(":red[Rating for First movie]",
+            min_value = 1,
+            max_value = 5)
+        col5, col6, col7, col8 = st.columns(4)
+        movie1 = col6.selectbox(":red[Second movie]", ratings.columns, key = "second movie")
+        rating1 = col7.number_input(":red[Rating for Second movie]",
+            min_value = 1,
+            max_value = 5,
+        key = "rating for second movie")
+        col9, col10, col11, col12 = st.columns(4)
+        movie2 = col10.selectbox(":red[Third movie]", ratings.columns, key = "third movie")
+        rating2 = col11.number_input(":red[Rating for Third movie]",
+            min_value = 1,
+            max_value = 5)
+    
+        user_input = {
+            "movie0": movie0,
+            "rating0": int(rating0),
+            "movie1": (movie1),
+            "rating1": int(rating1),
+            "movie2": movie2,
+            "rating2": int(rating2),
+            }
 
-#     @st.cache
-#     def load_model():
-#         with open(MODEL_FILE, "rb") as file_in:
-#             clf_LR = pickle.load(file_in)
-#         return clf_LR
 
-#     clf_LR = load_model()
 
-#     col1, col2, col3 = st.columns(3)
-
-#     gender = col1.number_input(
-#         "Gender (Type '1' for 'female' and '0' otherwise)", 
-#         min_value = 0,
-#         max_value = 1,
-#         )
-#     age = col2.number_input(
-#         "Age", 
-#         min_value = 1,
-#         max_value = 100,
-#         )
-#     siblings = col3.slider(
-#         "How many siblings does the passenger have? (Choose between '0' and '8')", 
-#         min_value = 0,
-#         max_value = 8,
-#         step = 1
-#         )
-
-#     col4, col5, col6 = st.columns(3)
-
-#     parch = col4.number_input(
-#         "How many family member (parents/children) were aboard?", 
-#         min_value = 1,
-#         max_value = 10,
-#         )
-#     female_pclass = col5.slider(
-#         "If female: which class did the passenger travel in?", 
-#         min_value = 0,
-#         max_value = 3,
-#         step = 1
-#         )
-#     male_pclass = col6.slider(
-#         "If male: which class did the passenger travel in?", 
-#         min_value = 0,
-#         max_value = 3,
-#         step = 1,
-#         key=101
-#         )
-
-#     user_input = {
-#             "gender": int(gender),
-#             "age": int(age),
-#             "siblings": int(siblings),
-#             "parch": int(parch),
-#             "female_pclass": int(female_pclass),
-#             "male_pclass": int(male_pclass),
-#             }
-
-#     user_input = pd.DataFrame(
-#         user_input, 
-#         columns = user_input.keys(), 
-#         index = [0])
-
-#     user_input = np.array(user_input).reshape(1, -1)
-
-#     pred = clf_LR.predict(user_input)[0]
-#     proba = clf_LR.predict_proba(user_input)[0]
-
-#     if st.button("Predict"):
-#         st.success(f'This person would have survived with a probability of: {round(proba[1], 2)}')
+    if st.button('Get recommendation'):
+        if model_choice == 'Random':
+            st.write(':red[This is Random]')
+            st.write(pd.DataFrame(recommend_random()))
+        elif model_choice == 'NMF':
+            st.write(':red[This is NMF]')
+            st.write(pd.DataFrame(recommend_with_NMF(query=user_input)))
+        else:
+            st.write(':red[This is Cosine]')
+            st.write(pd.DataFrame(recommend_neighborhood(query=user_input)))
 
 
 
 
+if nav == "Wombats":
+    st.title(':blue[Ciao Cacao]')
+    st.image('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fc.tenor.com%2Fv_CRiqOnPpIAAAAC%2Fwombat-eating.gif&f=1&nofb=1&ipt=7f5599fb69eee74da6e82f3972ba281588135a4e457d99b88cb78e8ae2f11636&ipo=images')
+    st.image('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fimages%2F19994749f3c8f7574ab5ddf8b925a1ec%2Ftenor.gif%3Fitemid%3D8758672&f=1&nofb=1&ipt=c0bb654cab47fddbfe7c4a87bc37ffaaceb2b47a56d6520de8a4f4658c51d83a&ipo=images')
+    st.image('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.giphy.com%2Fmedia%2F14miSV6VMiO7te%2Fgiphy.gif&f=1&nofb=1&ipt=2dce481028513f4fe070a6f0d491621c06a793ec8f85e93f899730432dfe9ece&ipo=images')
